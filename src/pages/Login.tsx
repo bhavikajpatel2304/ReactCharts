139505ec057219b6005543eec0d18a3fe9da9a38
ud17 <upandya1297@gmail.com>
2023-02-07 16:46:36 -0500

    login page design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import "../styles/Login.css";
+import bug from "../images/logo.png";
 
 function Login() {
   return (
-    <div>Login</div>
+      <div>
+        <div className="split left left-ratio">
+        <div className="centered">
+          <img src={bug}/>
+          <h2>Creepy Crawly</h2>
+          <h6>@{new Date().getFullYear()} Creepy Crawly</h6>
+        </div>
+      </div>
+      <div className="split right grad right-ratio">
+        <div className="container" id="container">
+            <div>
+                <form action="#">
+                    <h1>Sign in</h1>
+                    <input id="email" type="email" required placeholder="Email"/>
+                    <span>We don't share your email with anyone.</span>
+                    <input id="password" type="password" required placeholder="Password"/>
+                    <a href="#">Forgot your password?</a>
+                    <button>Identify Me</button>
+                </form>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
