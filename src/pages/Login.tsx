1d1a6e467b205cbc717544d753851f79041dc0c3
ud17 <upandya1297@gmail.com>
2023-02-13 14:26:39 -0500

    udit: login api integrated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,70 @@
 import "../styles/Login.css";
 import bug from "../images/logo.png";
+import axios from "axios";
+import { useState } from "react";
+import Alert from '@mui/material/Alert';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailChange = (email: string) => {
+    setEmail(email);
+    setError("");
+  }
+
+  const handlePasswordChange = (password: string) => {
+    setPassword(password);
+    setError("");
+  }
+
+  const handleLogin = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    // using context api, set universal token for global usage
+    let body = {
+      email: email,
+      password: password
+    }
+
+    await axios.post("http://127.0.0.1:5000/udit/login", body).then((response) => {
+        if(response.data.code === 200) {
+          navigate("/dashboard");
+        } 
+      }).catch((err) => {
+        if(err.response) {
+          setError(err?.response.data.message);
+        }
+      });
+  }
+
   return (
       <div>
         <div className="split left left-ratio">
         <div className="centered">
           <img src={bug}/>
           <h2>Creepy Crawly</h2>
-          <h6>@{new Date().getFullYear()} Creepy Crawly</h6>
+          <h6>&#169; {new Date().getFullYear()} Creepy Crawly</h6>
         </div>
       </div>
       <div className="split right grad right-ratio">
         <div className="container" id="container">
             <div>
-                <form action="#">
+                <form action="/">
                     <h1>Sign in</h1>
-                    <input id="email" type="email" required placeholder="Email"/>
+                    <input id="email" type="email" required placeholder="Email" onChange={(e) => handleEmailChange(e.target.value)}/>
                     <span>We don't share your email with anyone.</span>
-                    <input id="password" type="password" required placeholder="Password"/>
+                    <input id="password" type="password" required placeholder="Password" onChange={(e) => handlePasswordChange(e.target.value)}/>
                     <a href="#">Forgot your password?</a>
-                    <button>Identify Me</button>
+                    <button onClick={(e) => handleLogin(e)}>Identify Me</button>
                 </form>
+                {error !== "" && <Alert severity="warning">
+                          {error}
+                      </Alert>
+                }
             </div>
         </div>
       </div>
