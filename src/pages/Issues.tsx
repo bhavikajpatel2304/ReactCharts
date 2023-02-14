34b8e9c06b135660d7fc7499eabc51777f7426a5
Abhishek Kalhan <abhishekkalhan@Abhisheks-MacBook-Air.local>
2023-02-14 18:46:39 -0500

    Added Issues Page

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -1,12 +1,17 @@
+import { DataGrid } from '@mui/x-data-grid';
 import React from 'react'
 import Drawer from "../components/Drawer";
+import DataTable from '../components/Table';
 
 
 function Issues() {
   return (
     <div className="page-main">
     <Drawer />
-    <div className="page-body">Issues</div>
+    <div className="page-body">
+      <h1 className="issue-heading">All Issues</h1>
+    <DataTable />
+    </div>
   </div>
   )
 }
